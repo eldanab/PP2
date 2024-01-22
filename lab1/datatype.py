f69084ca72f67a635744ead3572308b3c8fb07ca
eldanab <y_balseit@kbtu.kz>
2024-01-22 15:23:06 +0600

    1

---
+++ lab1/datatype.py
@@ -0,0 +1,14 @@
+x = 5
+print(type(x)) #int
+y = "Hello World"
+print(type(y)) #string
+z = 20.5
+print(type(z)) #float
+a = ["apple", "banana", "cherry"]
+print(type(a)) #list
+b = ("apple", "banana", "cherry")
+print(type(b)) #tuple
+c = {"name" : "John", "age" : 36}
+print(type(c)) #dict
+d = True
+print(type(d)) #bool
---
+++ lab1/num.py
@@ -0,0 +1,11 @@
+x = 5
+x = float(x)
+print(x)
+
+y = 5.5
+y = int(y)
+print(y)
+
+z = 5
+z = complex(z)
+print(z)
