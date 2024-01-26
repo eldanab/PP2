93bedd3f0875e7813928bb2089ce5d3fa8a929ec
eldanab <y_balseit@kbtu.kz>
2024-01-26 10:20:53 +0600

    2

---
+++ lab2/for.py
@@ -0,0 +1,22 @@
+#Ex.1
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x)
+print()
+#Ex.2
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  if x == "banana":
+    continue
+  print(x)
+print()
+#Ex.3
+for x in range(6):
+  print(x)
+print()
+#Ex.4
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  if x == "banana":
+    break
+  print(x)
